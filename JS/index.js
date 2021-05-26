e96ca171b47b16fbb286abbb82938365129c6d0c
Maddie <mmsharris@gmail.com>
2021-05-26 12:13:49 -0400

    v.2.0
    Linked to HW20

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+//START NAVIGATION//
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+//END NAVIGATION//
